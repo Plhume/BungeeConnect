7b4d70eca87f60ee042e0d84982bc9a1d5ae67ab
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-18 16:47:16 +0100

    Remove comments and onDisable

--- src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
+++ src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
@@ -23,15 +23,11 @@ public final class BungeeConnect extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         saveDefaultConfig();
 
         getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     public String getServerName() {
         return this.serverName;
     }
