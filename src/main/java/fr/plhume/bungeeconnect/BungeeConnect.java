80020136250ae28bd7a8eec45a2d66e08017f4b0
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-18 16:46:01 +0100

    Update BungeeConnect.java

--- src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
+++ src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BungeeConnect extends JavaPlugin {
 
+    private String serverName;
+    private String[] servers;
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -15,5 +17,21 @@ public final class BungeeConnect extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    public String getServerName() {
+        return this.serverName;
+    }
+
+    public void setServerName() {
+        this.serverName = serverName;
+    }
+
+    public void setServers(String[] servers) {
+        this.servers = servers;
+    }
+
+
+    public String[] getServers() {
+        return this.servers;
+    }
     }
 }
