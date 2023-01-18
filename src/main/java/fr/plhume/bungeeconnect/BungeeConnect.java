9b2b5f1b01f862d3e2acd5adc88de057d31af567
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-18 16:46:56 +0100

    Create Teleport Functions

--- src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
+++ src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
@@ -1,7 +1,15 @@
 package fr.plhume.bungeeconnect;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOError;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -40,6 +48,38 @@ public final class BungeeConnect extends JavaPlugin {
     public String[] getServers() {
         return this.servers;
     }
+
+    public void sendServer(Player player, String serverName) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(b);
+
+        try {
+            out.writeUTF("Connect");
+            out.writeUTF(serverName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        player.sendMessage(ChatColor.DARK_GREEN + "[BungeeConnect] " + ChatColor.GOLD + "you will be connect to server " + ChatColor.WHITE + serverName);
+        player.sendPluginMessage(this, "BungeeCord", b.toByteArray());
+    }
+
+    public void tpPlayer(Player player, Location location) {
+        if (player != null && teleportMap.containsKey(player.getUniqueId())) {
+            player.teleport(location);
+            teleportMap.remove(player.getUniqueId());
+        }
+    }
+
+    public void tpPlayer(Player player, Location location, String serverName, String worldName) {
+        if (!serverName.equals(this.getServerName())) {
+            this.sendServer(player, serverName);
+        } else {
+            location.setWorld(Bukkit.getWorld(worldName));
+            player.teleport(location);
+        }
+    }
+
     public Map<UUID, Location> getTeleportMap() {
         return teleportMap;
     }
