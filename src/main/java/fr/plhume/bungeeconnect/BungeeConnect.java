84c365db4e44e14748f15009f00225e7d3d03e94
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-18 16:46:28 +0100

    Add Maps

--- src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
+++ src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
@@ -2,10 +2,17 @@ package fr.plhume.bungeeconnect;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public final class BungeeConnect extends JavaPlugin {
 
     private String serverName;
     private String[] servers;
+    private final Map<UUID, String> mapChangeName = new HashMap<>();
+    private final Map<UUID, Location> teleportMap = new HashMap<>();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -33,5 +40,11 @@ public final class BungeeConnect extends JavaPlugin {
     public String[] getServers() {
         return this.servers;
     }
+    public Map<UUID, Location> getTeleportMap() {
+        return teleportMap;
+    }
+
+    public Map<UUID, String> getMapChangeName() {
+        return this.mapChangeName;
     }
 }
