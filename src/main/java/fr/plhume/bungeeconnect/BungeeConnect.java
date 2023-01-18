5ab7e43b9ea99a9d9c597f49bd681674cf21402b
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-18 16:45:00 +0100

    Add PluginMessages

--- src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
+++ src/main/java/fr/plhume/bungeeconnect/BungeeConnect.java
@@ -8,6 +8,7 @@ public final class BungeeConnect extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
     }
 
     @Override
