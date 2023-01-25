eb6df3010063d2f1278cc3c788cf92e522c321f9
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-25 16:49:45 +0100

    Creating tp request function

--- src/main/java/fr/plhume/bungeeconnect/managers/PluginMessageManager.java
+++ src/main/java/fr/plhume/bungeeconnect/managers/PluginMessageManager.java
@@ -34,4 +34,33 @@ public class PluginMessageManager {
         }
     }
 
+    public void sendTeleportRequest(Player player, Location location, String server, String worldName) {
+
+        if (player == null) {
+            return;
+        }
+
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("Forward");
+        out.writeUTF(server);
+        out.writeUTF("Teleport");
+
+        ByteArrayOutputStream msgbytes = new ByteArrayOutputStream();
+        DataOutputStream msgout = new DataOutputStream(msgbytes);
+
+        try {
+            msgout.writeUTF(player.getUniqueId().toString());
+            msgout.writeUTF(worldName);
+            msgout.writeDouble(location.getX());
+            msgout.writeDouble(location.getY());
+            msgout.writeDouble(location.getZ());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        out.writeShort(msgbytes.toByteArray().length);
+        out.write(msgbytes.toByteArray());
+
+        player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+    }
 }
