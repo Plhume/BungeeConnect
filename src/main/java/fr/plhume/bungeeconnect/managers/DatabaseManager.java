cea80c62e23d71b45af3595bc0053b93b5685d50
Plhume <felix@exodius.fr>
2023-01-25 23:22:56 +0100

    Create DatabaseManager.java

---
+++ src/main/java/fr/plhume/bungeeconnect/managers/DatabaseManager.java
@@ -0,0 +1,14 @@
+package fr.plhume.bungeeconnect.managers;
+
+import fr.plhume.bungeeconnect.BungeeConnect;
+import org.bukkit.configuration.Configuration;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author Plhume
+ */
+public class DatabaseManager {
+}
