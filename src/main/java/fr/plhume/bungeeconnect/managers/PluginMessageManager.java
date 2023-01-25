9cafdc7cc1667d70c76ba8826cdc4debc52689ea
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-01-25 16:49:14 +0100

    Update PluginMessageManager.java

--- src/main/java/fr/plhume/bungeeconnect/managers/PluginMessageManager.java
+++ src/main/java/fr/plhume/bungeeconnect/managers/PluginMessageManager.java
@@ -1,5 +1,37 @@
 package fr.plhume.bungeeconnect.managers;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import fr.plhume.bungeeconnect.BungeeConnect;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class PluginMessageManager {
+
+    private final BungeeConnect plugin;
+
+    public PluginMessageManager(BungeeConnect plugin) {
+        this.plugin = plugin;
+    }
+
+    public void getServers(Player player) {
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("GetServers");
+        if (player != null) {
+            player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+        }
+    }
+
+    public void getServerName(Player player) {
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("GetServer");
+        if (player != null) {
+            player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+        }
+    }
 
 }
