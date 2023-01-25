9e8acfdc44b168148e83afde899326be6635db8b
Plhume <felix@exodius.fr>
2023-01-25 23:23:46 +0100

    Adding connect function

--- src/main/java/fr/plhume/bungeeconnect/managers/DatabaseManager.java
+++ src/main/java/fr/plhume/bungeeconnect/managers/DatabaseManager.java
@@ -24,6 +24,22 @@ public class DatabaseManager {
     public DatabaseManager(BungeeConnect plugin) throws SQLException, IOException {
         this.plugin = plugin;
         Configuration dbConfig = plugin.getConfig();
+        connect(dbConfig);
+    }
+
+    private void connect(Configuration dbInfos) throws SQLException {
+        String host = dbInfos.getString("database.host");
+        String port = dbInfos.getString("database.port");
+        String dbName = dbInfos.getString("database.database");
+        String username = dbInfos.getString("database.user");
+        String password = dbInfos.getString("database.password");
+
+        jdbcString = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+        DBUsername = username;
+        DBPassword = password;
+
+        serverTable = dbInfos.getString("database.tables.servers");
+        worldTable = dbInfos.getString("database.tables.worlds");
     }
 
 }
